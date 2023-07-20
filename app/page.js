81d91e06f8cefa6fd8694f361d1f50396870aec6
wissam_6 <wissammerhi@abv.bg>
2023-07-21 02:47:34 +0300

    feat(home): add homepage

--- app/page.js
+++ app/page.js
@@ -5,7 +5,9 @@ import Link from "next/link";
 export default function Page() {
   return (
     <React.Fragment>
-      <h1>Welcome to Globomantics App</h1>
+      <h1>Golden Perfume</h1>
+      <Link href="/home">Home</Link>
+      <br/>
       <Link href="/authentication/signin">Sign In</Link>
       <br/>
       <Link href="/authentication/signup">Sign Up</Link>
