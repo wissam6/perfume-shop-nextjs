01ef5e54730eac63a3f7c67172247fdef6d8bd04
wissam_6 <wissammerhi@abv.bg>
2023-08-13 00:05:05 +0300

    chore: use env variables

---
+++ app/firebase.ts
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+//import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: process.env.apiKey,
+  authDomain: process.env.authDomain,
+  projectId: process.env.projectId,
+  storageBucket: process.env.storageBucket,
+  messagingSenderId: process.env.messagingSenderId,
+  appId: process.env.appId,
+  measurementId: process.env.measurementId,
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
