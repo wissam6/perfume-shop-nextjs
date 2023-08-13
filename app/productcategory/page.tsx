bf2033d135e18dde20fdb711e1cf11affedc3728
wissam_6 <wissammerhi@abv.bg>
2023-08-13 15:41:16 +0300

    fix: resolve page issue

--- app/productcategory/page.tsx
+++ app/productcategory/page.tsx
@@ -5,11 +5,11 @@ import { useSearchParams } from "next/navigation";
 
 const product = "versace";
 
-export const Page = (props) => {
+export const ProductPage = (props) => {
   /* const searchParams = useSearchParams();
   const search = searchParams.get("search");
   console.log(search); */
   return <ProductsCategory product={product} />;
 };
 
-export default Page;
+export default ProductPage;
