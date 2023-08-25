342b3d28ded094980a9528e42d2e373390b508d3
wissam_6 <wissammerhi@abv.bg>
2023-08-25 16:27:28 +0300

    add header to home page

--- app/page.js
+++ app/page.js
@@ -2,10 +2,11 @@
 
 import * as React from "react";
 import { Home } from "./home/Home";
-
+import { Header } from "./components/Home/Header";
 export default function Page() {
   return (
     <React.Fragment>
+      <Header/>
       <Home/>
     </React.Fragment>
   );
