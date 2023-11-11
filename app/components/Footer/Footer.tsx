048b2e003220759236a984b72fbd99f06f79a840
wissam_6 <wissammerhi@abv.bg>
2023-11-12 00:04:18 +0200

    fix footer styles

--- app/components/Footer/Footer.tsx
+++ app/components/Footer/Footer.tsx
@@ -9,7 +9,7 @@ export const Footer = () => {
           bottom: 0,
           height: "100%",
           width: "100%",
-          backgroundColor: "lightgrey",
+          backgroundColor: "rgb(32, 37, 108)",
           display: "flex",
           flexWrap: "wrap",
           position: "relative",
