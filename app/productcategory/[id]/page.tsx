614c3712083920862a7a36c0e91271936b6b950d
wissam_6 <wissammerhi@abv.bg>
2023-11-27 00:36:18 +0200

    product loading

--- app/productcategory/[id]/page.tsx
+++ app/productcategory/[id]/page.tsx
@@ -1,10 +1,9 @@
 import * as React from "react";
 import { ProductsCategory } from "../product-category";
-import { ProductLoader } from "../products-loader";
 
 //import { useSearchParams } from "next/navigation";
 
-export default function Page({ params }: { params: { id: string } }) {
+export default async function Page({ params }: { params: { id: string } }) {
   const product = params.id.toLowerCase();
 
   return <ProductsCategory product={product} />;
