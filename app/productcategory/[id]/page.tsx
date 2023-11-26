68e65caabf1792f998c4718b62df7803015ac4a4
wissam_6 <wissammerhi@abv.bg>
2023-11-27 01:27:03 +0200

    refactor product loading

--- app/productcategory/[id]/page.tsx
+++ app/productcategory/[id]/page.tsx
@@ -1,10 +1,25 @@
 import * as React from "react";
-import { ProductsCategory } from "../product-category";
-
-//import { useSearchParams } from "next/navigation";
+import { ProductsCategoryClient } from "../ProductsCategory";
+import { db } from "../../firebase";
+import { collection, getDocs } from "firebase/firestore";
 
 export default async function Page({ params }: { params: { id: string } }) {
   const product = params.id.toLowerCase();
 
-  return <ProductsCategory product={product} />;
+  const getData = async () => {
+    //to be replaced by fetching only the needed category
+    let products: any = [];
+    const querySnapshot = await getDocs(collection(db, "perfumes"));
+    querySnapshot.forEach((doc) => {
+      let docData = doc.data();
+      docData.id = doc.id;
+      if (docData.brand.toLowerCase() === product) {
+        products = [...products, docData];
+      }
+    });
+    return products;
+  };
+  const data = await getData();
+
+  return <ProductsCategoryClient product={product} data={data} />;
 }
--- app/productcategory/products-loader.tsx
+++ app/productcategory/products-loader.tsx
@@ -1,18 +1,96 @@
 "use client";
 import * as React from "react";
-
+import { GridLayout, GridLayoutItem } from "@progress/kendo-react-layout";
+import {
+  Card,
+  CardHeader,
+  CardTitle,
+  CardBody,
+  CardActions,
+  CardImage,
+  CardSubtitle,
+  Avatar,
+} from "@progress/kendo-react-layout";
+import { Button } from "@progress/kendo-react-buttons";
 import { Loader } from "@progress/kendo-react-indicators";
 
 export const ProductLoader = () => {
   return (
-    <div
-      style={{
-        margin: "0 auto",
-        width: "50%",
-      }}
-    >
-      Loading...
-      <Loader size="large" type={"infinite-spinner"} />
-    </div>
+    <React.Fragment>
+      <h3
+        style={{
+          fontSize: "80px",
+          paddingLeft: "2em",
+        }}
+      ></h3>
+      <div
+        style={{
+          paddingLeft: "10em",
+          paddingBottom: "8em",
+        }}
+      >
+        <GridLayout
+          gap={{
+            rows: 20,
+            cols: 40,
+          }}
+          cols={[
+            {
+              width: "30%",
+            },
+            {
+              width: "30%",
+            },
+            {
+              width: "30%",
+            },
+          ]}
+        >
+          {[1, 2, 3].map((index: number) => {
+            return (
+              <GridLayoutItem key={index}>
+                <Card
+                  orientation="horizontal"
+                  style={{
+                    height: "300px",
+                  }}
+                >
+                  <CardImage />
+                  <div className="k-vbox">
+                    <CardHeader>
+                      <CardTitle></CardTitle>
+                      <CardSubtitle></CardSubtitle>
+                    </CardHeader>
+                    <CardBody>
+                      <ul
+                        style={{
+                          listStyle: "none",
+                          paddingLeft: "0px",
+                        }}
+                      >
+                        <Loader size="large" type={"infinite-spinner"} />
+                        <li>price: </li>
+                        <li>rating: </li>
+                        <li>sex: </li>
+                        <li>size: </li>
+                        <li>remaining: </li>
+                      </ul>
+                    </CardBody>
+                    <CardActions>
+                      <Button className="k-button k-button-md k-rounded-md k-button-flat k-button-flat-primary">
+                        Add to Cart
+                      </Button>
+                      <Button className="k-button k-button-md k-rounded-md k-button-flat k-button-flat-primary">
+                        Favorite
+                      </Button>
+                    </CardActions>
+                  </div>
+                </Card>
+              </GridLayoutItem>
+            );
+          })}
+        </GridLayout>
+      </div>
+    </React.Fragment>
   );
 };
