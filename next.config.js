31bbe237c4a7dbc8ff82bf92d6544b88e00c09ab
wissam_6 <wissammerhi@abv.bg>
2023-12-07 15:52:34 +0200

    env variable

--- next.config.js
+++ next.config.js
@@ -18,6 +18,7 @@ const nextConfig = {
         MESSAGING_SENDER_ID: process.env.MESSAGING_SENDER_ID,
         APP_ID: process.env.APP_ID,
         MEASUREMENT_ID: process.env.MEASUREMENT_ID,
+        KENDO_UI_LICENSE: process.env.KENDO_UI_LICENSE
     }
 }
 
