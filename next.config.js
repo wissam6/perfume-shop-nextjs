cb2706416df89d168d641abc8b2df6023fd3333d
wissam_6 <wissammerhi@abv.bg>
2023-12-07 16:17:33 +0200

    remove variable

--- next.config.js
+++ next.config.js
@@ -17,8 +17,7 @@ const nextConfig = {
         STORAGE_BUCKET: process.env.STORAGE_BUCKET,
         MESSAGING_SENDER_ID: process.env.MESSAGING_SENDER_ID,
         APP_ID: process.env.APP_ID,
-        MEASUREMENT_ID: process.env.MEASUREMENT_ID,
-        KENDO_UI_LICENSE: process.env.KENDO_UI_LICENSE
+        MEASUREMENT_ID: process.env.MEASUREMENT_ID
     }
 }
 
